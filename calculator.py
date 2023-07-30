1b6de833c00dadcaec32eafca4c9327744911584
bsrnremr <busranuremiroglu@gmail.com>
2023-07-30 20:49:17 +0100

    calculator ekledim

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def calculator(a,b,x):
+    if x == "+":
+        return a + b
+    elif x == "-":
+        return a - b 
+    elif x == "*":
+        return a * b
+    elif x == "/":
+        return a / b
+    else: "farkli bir islem dene" 
+
+calculator(5,6,"+")
+
+    
